ad8d610c25c5a822e89d91d1d94c9aaefbaf8820
Suman Humagain <returnzerosh@gmail.com>
2024-10-16 18:34:57 -0400

    Added model and dbcontext

---
+++ Inventory Management System/Models/InventoryMSDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_Management_System.Models
+{
+    public class InventoryMSDbContext : DbContext
+    {
+        public InventoryMSDbContext(DbContextOptions<InventoryMSDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+
+}
---
+++ Inventory Management System/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventory_Management_System.Models
+{
+    public class Product
+    {
+        public string ProductID { get; set; } // PK
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
