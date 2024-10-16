02b7f9da66d0f8b2e9735a7e85e49f8ed6632d9f
Suman Humagain <returnzerosh@gmail.com>
2024-10-16 18:19:29 -0400

    Added models

---
+++ Inventory Management System/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace Inventory_Management_System.Models
+{
+    public class Customer
+    {
+        public string CustomerID { get; set; } // PK
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
