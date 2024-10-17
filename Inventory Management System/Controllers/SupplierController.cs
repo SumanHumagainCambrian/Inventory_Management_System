f97004276a1caa614c0804652f314fd91f9f6510
A00303235 <A00303235@mycambrian.ca>
2024-10-17 16:34:10 -0400

    Codded Supplier Controller as needed

--- Inventory Management System/Controllers/SupplierController.cs
+++ Inventory Management System/Controllers/SupplierController.cs
@@ -1,12 +1,81 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Inventory_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Inventory_Management_System.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class SupplierController : Controller
     {
-        public IActionResult Index()
+        private readonly InventoryMSDbContext _context;
+
+        public SupplierController(InventoryMSDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            return await _context.Suppliers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Supplier>> GetSupplier(int id)
+        {
+            var supplier = await _context.Suppliers.FindAsync(id);
+
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return supplier;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Supplier>> PostSupplier(Supplier supplier)
+        {
+            _context.Suppliers.Add(supplier);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSupplier), new { id = supplier.SupplierId }, supplier);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSupplier(int id, Supplier supplier)
+        {
+            if (supplier == null)
+            {
+                return BadRequest();
+            }
+
+            if (id != supplier.SupplierId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(supplier).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSupplier(int id)
+        {
+            var supplier = await _context.Suppliers.FindAsync(id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            _context.Suppliers.Remove(supplier);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
