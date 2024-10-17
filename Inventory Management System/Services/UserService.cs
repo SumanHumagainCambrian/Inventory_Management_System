a03043853ed30015e07d9e3ca6ed9514b7a3efe5
Suman Humagain <returnzerosh@gmail.com>
2024-10-17 17:51:03 -0400

    for security purpose saved hashed password using SHA256 encryption

--- Inventory Management System/Services/UserService.cs
+++ Inventory Management System/Services/UserService.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Inventory_Management_System.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Inventory_Management_System.Services
 {
@@ -22,9 +24,27 @@ namespace Inventory_Management_System.Services
 
         public async Task<ActionResult<User>> CreateUserAsync(User user)
         {
+            user.Password = HashPassword(user.Password);
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            return user;  
+            return user;
         }
+
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
     }
 }
