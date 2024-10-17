07d4e8162babeb798a5f5b477f7f02bbfbab92b9
Ankita <A00305054@mycambrian.ca>
2024-10-17 18:29:37 -0400

    Create supply class

--- Inventory Management System/Models/Supplier.cs
+++ Inventory Management System/Models/Supplier.cs
@@ -1,6 +1,25 @@
-﻿namespace Inventory_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management_System.Models
 {
     public class Supplier
     {
+        [Key]
+        public int SupplierId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string SupplierName { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string ContactPerson { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(250)]
+        public string Address { get; set; }
     }
 }
