5d53415b9aaa6d4cd3042336efc7427835b3a1bf
Ankita <A00305054@mycambrian.ca>
2024-10-16 21:36:06 -0400

    Created customer class model

--- Inventory Management System/Models/Product.cs
+++ Inventory Management System/Models/Product.cs
@@ -1,1 +1,11 @@
-﻿
+﻿namespace Inventory_Management_System.Models
+{
+    public class Product
+    {
+        public string ProductID { get; set; } // PK
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
