1ef64050a63fff8de51ef86e10b36d14c791e386
Suman Humagain <returnzerosh@gmail.com>
2024-10-17 11:43:10 -0400

    Added warehouse model

---
+++ Inventory Management System/Models/Warehouse.cs
@@ -0,0 +1,9 @@
+﻿namespace Inventory_Management_System.Models
+{
+    public class Warehouse
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
