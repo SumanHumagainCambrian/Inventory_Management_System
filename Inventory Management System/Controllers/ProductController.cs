4a6eb14c2a0046b4875609fc67df710eb3bcc3b0
Suman Humagain <returnzerosh@gmail.com>
2024-10-17 11:15:44 -0400

    completed product add,edit,delete

--- Inventory Management System/Controllers/ProductController.cs
+++ Inventory Management System/Controllers/ProductController.cs
@@ -22,5 +22,59 @@ namespace Inventory_Management_System.Controllers
             return _context.Products.ToList();
         }
 
+        // GET: api/Product/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(string id)
+        {
+            var product = _context.Products.Find(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public ActionResult<Product> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetProduct", new { id = product.ProductID }, product);
+        }
+
+        // PUT: api/Product/{id}
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(string id, Product product)
+        {
+            if (id != product.ProductID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Product/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(string id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
