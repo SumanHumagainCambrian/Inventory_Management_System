d6db463b599773e72665930995bd13e5f8a12b10
A00303235 <A00303235@mycambrian.ca>
2024-10-17 18:02:45 -0400

    Created OrderItem model and codded it and edited Supplier Model

---
+++ Inventory Management System/Models/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management_System.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int OrderItemId { get; set; } 
+        public int OrderId { get; set; } 
+        public int ProductId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
--- Inventory Management System/Models/Supplier.cs
+++ Inventory Management System/Models/Supplier.cs
@@ -1,12 +1,20 @@
-﻿namespace Inventory_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management_System.Models
 {
     public class Supplier
     {
+        [Key]
         public int SupplierId { get; set; }
+        [Required]
         public string Name { get; set; }
-        public string Email { get; set; }   
+        [Required]
+        public string Email { get; set; } 
+        [Required]
         public string Phone { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string CompanyName { get; set; }
     }
 }
