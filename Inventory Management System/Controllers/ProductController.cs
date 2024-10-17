62fe80cee0a99eb0fa0a20de0515bd60b465c434
Suman Humagain <returnzerosh@gmail.com>
2024-10-17 11:09:05 -0400

    Implementing product controller CRUD

---
+++ Inventory Management System/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Inventory_Management_System.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly InventoryMSDbContext _context;
+
+        public ProductController(InventoryMSDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Product
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+    }
+}
