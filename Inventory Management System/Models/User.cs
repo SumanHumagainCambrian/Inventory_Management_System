3998ecbdb26e362d800f8eb05b1b63de50775dd9
Suman Humagain <returnzerosh@gmail.com>
2024-10-17 11:47:56 -0400

    Update models

--- Inventory Management System/Models/User.cs
+++ Inventory Management System/Models/User.cs
@@ -1,10 +1,18 @@
-﻿namespace Inventory_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management_System.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
         public string Username { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Required]
         public string Role { get; set; }
 
     }
--- Inventory Management System/Models/Warehouse.cs
+++ Inventory Management System/Models/Warehouse.cs
@@ -1,9 +1,15 @@
-﻿namespace Inventory_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management_System.Models
 {
     public class Warehouse
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Location { get; set; }
+
+        [Required]
         public List<Product> Products { get; set; }
     }
 }
