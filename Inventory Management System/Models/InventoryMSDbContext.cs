a0ab0b6b705b618d2b64218e2e324149f11999e1
Ankita <A00305054@mycambrian.ca>
2024-10-16 22:50:14 -0400

    Created new order class in model

--- Inventory Management System/Models/InventoryMSDbContext.cs
+++ Inventory Management System/Models/InventoryMSDbContext.cs
@@ -4,12 +4,15 @@ namespace Inventory_Management_System.Models
 {
     public class InventoryMSDbContext : DbContext
     {
-        public InventoryMSDbContext(DbContextOptions<InventoryMSDbContext> options) : base(options) { }
+        public InventoryMSDbContext(DbContextOptions<InventoryMSDbContext> options) : base(options)
+        {
+        }
 
+        public DbSet<Customer> Customers { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<User> Users { get; set; }
-        public DbSet<Customer> Customers { get; set; }
 
+        // Add the DbSet for Orders
+        public DbSet<Order> Orders { get; set; }
     }
-
 }
---
+++ Inventory Management System/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management_System.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        // Navigation property to the Customer model
+        public Customer Customer { get; set; }
+
+        // List of products in the order
+        public List<Product> Products { get; set; }
+
+        [Required]
+        public decimal TotalAmount { get; set; }
+    }
+}
