5d75051075d0fa4ea14421524e0fa3e3c173ed09
Ankita <A00305054@mycambrian.ca>
2024-10-17 20:22:33 -0400

    modify Dbcontext file

--- Inventory Management System/Models/InventoryMSDbContext.cs
+++ Inventory Management System/Models/InventoryMSDbContext.cs
@@ -11,6 +11,8 @@ namespace Inventory_Management_System.Models
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
 
         // Add the DbSet for Orders
         public DbSet<Order> Orders { get; set; }
